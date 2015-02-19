d8baca113d01c35227aa4724869e7eb078451e34
Frederik <girafi0@gmail.com>
2015-02-19 03:58:33 +0100

    Made Lamb and Veal only drop, if you use the knife

--- src/main/java/com/Girafi/culinarycultivation/event/BabyCowDropsVeal.java
+++ src/main/java/com/Girafi/culinarycultivation/event/BabyCowDropsVeal.java
@@ -3,34 +3,40 @@ package com.Girafi.culinarycultivation.event;
 import com.Girafi.culinarycultivation.init.ModItems;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.entity.passive.EntityCow;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
 
-public class BabyCowDropsVeal  {
+public class BabyCowDropsVeal {
 
     public static double rand;
 
     @SubscribeEvent
     public void LivingDropsEvent(LivingDropsEvent DropsEvent) {
+
         rand = Math.random();
-        if (DropsEvent.entityLiving instanceof EntityCow && DropsEvent.entityLiving.isChild()) {
-            if (rand < 0.13D) {
-                if (DropsEvent.entityLiving.isBurning()) {
-                    DropsEvent.entityLiving.dropItem(ModItems.cookedVeal, 2);
 
-                } else {
-                    DropsEvent.entityLiving.dropItem(ModItems.veal, 2);
+        if (DropsEvent.source.getSourceOfDamage() instanceof EntityPlayer) {
+            EntityPlayer player = (EntityPlayer) DropsEvent.source.getSourceOfDamage();
+            if (player.getCurrentEquippedItem() != null && player.getCurrentEquippedItem().getItem() == ModItems.knife) {
+                if (DropsEvent.entityLiving instanceof EntityCow && DropsEvent.entityLiving.isChild()) {
+                        if (DropsEvent.entityLiving.isBurning()) {
+                            DropsEvent.entityLiving.dropItem(ModItems.cookedVeal, 2);
+
+                        } else {
+                            DropsEvent.entityLiving.dropItem(ModItems.veal, 2);
+                        }
+                    }
                 }
-            }
-        }
-        if (DropsEvent.entityLiving instanceof EntityCow && DropsEvent.entityLiving.isChild()) {
-            if (rand > 0.7D) {
-                if (DropsEvent.entityLiving.isBurning()) {
-                    DropsEvent.entityLiving.dropItem(ModItems.cookedVeal, 1);
+                if (DropsEvent.entityLiving instanceof EntityCow && DropsEvent.entityLiving.isChild()) {
+                    if (rand > 0.7D) {
+                        if (DropsEvent.entityLiving.isBurning()) {
+                            DropsEvent.entityLiving.dropItem(ModItems.cookedVeal, 1);
 
-                } else {
-                    DropsEvent.entityLiving.dropItem(ModItems.veal, 1);
+                        } else {
+                            DropsEvent.entityLiving.dropItem(ModItems.veal, 1);
+                        }
+                    }
                 }
             }
         }
     }
-}
--- src/main/java/com/Girafi/culinarycultivation/event/BabySheepDropsLamb.java
+++ src/main/java/com/Girafi/culinarycultivation/event/BabySheepDropsLamb.java
@@ -1,8 +1,11 @@
 package com.Girafi.culinarycultivation.event;
 
 import com.Girafi.culinarycultivation.init.ModItems;
+import com.Girafi.culinarycultivation.item.ItemKnife;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.entity.passive.EntitySheep;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
 
@@ -12,27 +15,31 @@ public class BabySheepDropsLamb {
 
     @SubscribeEvent
     public void LivingDropsEvent(LivingDropsEvent DropsEvent) {
+
         rand = Math.random();
-        if (LivingDeathEvent.)
-        if (DropsEvent.entityLiving instanceof EntitySheep && DropsEvent.entityLiving.isChild()) {
-            if (rand < 0.08D) {
-                if (DropsEvent.entityLiving.isBurning()) {
-                    DropsEvent.entityLiving.dropItem(ModItems.cookedLamb, 2);
 
-                } else {
-                    DropsEvent.entityLiving.dropItem(ModItems.lamb, 2);
+        if (DropsEvent.source.getSourceOfDamage() instanceof EntityPlayer) {
+            EntityPlayer player = (EntityPlayer)DropsEvent.source.getSourceOfDamage();
+            if (player.getCurrentEquippedItem() != null && player.getCurrentEquippedItem().getItem() == ModItems.knife) {
+                if (DropsEvent.entityLiving instanceof EntitySheep && DropsEvent.entityLiving.isChild()) {
+                        if (DropsEvent.entityLiving.isBurning()) {
+                            DropsEvent.entityLiving.dropItem(ModItems.cookedLamb, 2);
+
+                        } else {
+                            DropsEvent.entityLiving.dropItem(ModItems.lamb, 2);
+                        }
+                    }
                 }
-            }
-        }
-        if (DropsEvent.entityLiving instanceof EntitySheep && DropsEvent.entityLiving.isChild()) {
-             if (rand > 0.65D) {
-                 if (DropsEvent.entityLiving.isBurning()) {
-                     DropsEvent.entityLiving.dropItem(ModItems.cookedLamb, 1);
+                if (DropsEvent.entityLiving instanceof EntitySheep && DropsEvent.entityLiving.isChild()) {
+                    if (rand > 0.8D) {
+                        if (DropsEvent.entityLiving.isBurning()) {
+                            DropsEvent.entityLiving.dropItem(ModItems.cookedLamb, 1);
 
-                 } else {
-                     DropsEvent.entityLiving.dropItem(ModItems.lamb, 1);
+                        } else {
+                            DropsEvent.entityLiving.dropItem(ModItems.lamb, 1);
                         }
                     }
                 }
             }
         }
+    }
