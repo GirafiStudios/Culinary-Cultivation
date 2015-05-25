a33b7d1b6a95ea64d90bdfa38ab9f4899eb0c825
GirafiStudios <girafi0@gmail.com>
2015-05-25 06:49:53 +0200

    Made it possible to make certain vanilla fishs into fillet too

--- src/main/java/com/Girafi/culinarycultivation/init/OreDictionaryRegistration.java
+++ src/main/java/com/Girafi/culinarycultivation/init/OreDictionaryRegistration.java
@@ -3,12 +3,12 @@ package com.Girafi.culinarycultivation.init;
 import com.Girafi.culinarycultivation.item.ItemModFishFood.FishType;
 import com.Girafi.culinarycultivation.item.ItemModMeatFood.MeatType;
 import net.minecraft.init.Items;
+import net.minecraft.item.ItemFishFood;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
 import org.apache.commons.lang3.text.WordUtils;
 
 public class OreDictionaryRegistration {
-
     public static void init() {
 
         FishType[] fish = FishType.values();
@@ -18,9 +18,13 @@ public class OreDictionaryRegistration {
             OreDictionary.registerOre("food" + WordUtils.capitalize(fishtype.getTextureName()) + "Raw", new ItemStack(ModItems.fish, 1, fishtype.getMetaData()));
             OreDictionary.registerOre("food" + WordUtils.capitalize(fishtype.getTextureName()) + "Cooked", new ItemStack(ModItems.cooked_fish, 1, fishtype.getMetaData()));
             if (fishtype.isHaveRawFish() && fishtype.getMetaData() != FishType.FILLET.getMetaData() && fishtype.getMetaData() != FishType.SMALLSQUID.getMetaData()) {
-                OreDictionary.registerOre("fish" , new ItemStack(ModItems.fish, 1, fishtype.getMetaData()));
+                OreDictionary.registerOre("fish", new ItemStack(ModItems.fish, 1, fishtype.getMetaData()));
             }
         }
+        OreDictionary.registerOre("fish", new ItemStack(Items.fish, 1, ItemFishFood.FishType.COD.func_150976_a()));
+        OreDictionary.registerOre("fish", new ItemStack(Items.fish, 1, ItemFishFood.FishType.SALMON.func_150976_a()));
+        OreDictionary.registerOre("fish", new ItemStack(Items.fish, 1, ItemFishFood.FishType.CLOWNFISH.func_150976_a()));
+
         MeatType[] meat = MeatType.values();
         int iMeat = meat.length;
         for (int j = 0; j < iMeat; ++j) {
@@ -40,7 +44,7 @@ public class OreDictionaryRegistration {
         OreDictionary.registerOre("foodMincedMeat", new ItemStack(ModItems.meat, 1, MeatType.HAM.getMetaData()));
         OreDictionary.registerOre("foodMincedMeat", ModItems.mutton);
         OreDictionary.registerOre("foodMincedMeat", new ItemStack(ModItems.meat, 1, MeatType.ROAST.getMetaData()));
-        OreDictionary.registerOre("foodMincedMeat", new ItemStack(ModItems.meat, 1,  MeatType.VEAL.getMetaData()));
+        OreDictionary.registerOre("foodMincedMeat", new ItemStack(ModItems.meat, 1, MeatType.VEAL.getMetaData()));
         OreDictionary.registerOre("foodMincedMeat", new ItemStack(ModItems.meat, 1, MeatType.LAMB.getMetaData()));
     }
 }
