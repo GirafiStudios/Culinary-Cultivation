1cbafe71279f5a3806f949e4c48a5082c74e1268
GirafiStudios <girafi0@gmail.com>
2016-07-04 12:31:42 +0200

    Build against MC 1.10.2 Moved gui factory stuff

--- src/main/java/com/girafi/culinarycultivation/util/reference/Reference.java
+++ src/main/java/com/girafi/culinarycultivation/util/reference/Reference.java
@@ -7,5 +7,5 @@ public class Reference {
     public static final String CLIENT_PROXY_CLASS = "com.girafi.culinarycultivation.proxy.ClientProxy";
     public static final String SERVER_PROXY_ClASS = "com.girafi.culinarycultivation.proxy.ServerProxy";
     public static final String DEPENDENCIES = "required-after:Forge@[12.18,)";
-    public static final String GUI_FACTORY_CLASS = "com.girafi.culinarycultivation.client.gui.GuiFactory";
+    public static final String GUI_FACTORY_CLASS = "com.girafi.culinarycultivation.client.gui.config.GuiFactory";
 }
