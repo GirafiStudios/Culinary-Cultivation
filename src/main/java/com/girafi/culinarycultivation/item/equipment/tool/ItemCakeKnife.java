4adacfbed972ec85ce014649d90c5ab4c763ceb8
GirafiStudios <girafi0@gmail.com>
2016-10-03 22:42:53 +0200

    Made the cake knife effective on cake material, instead of only the cake block

--- src/main/java/com/girafi/culinarycultivation/item/equipment/tool/ItemCakeKnife.java
+++ src/main/java/com/girafi/culinarycultivation/item/equipment/tool/ItemCakeKnife.java
@@ -3,22 +3,20 @@ package com.girafi.culinarycultivation.item.equipment.tool;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
 import com.google.common.collect.Sets;
-import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
-import net.minecraft.init.Blocks;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import java.util.Set;
 
 public class ItemCakeKnife extends ItemCraftTool {
-    private static final Set<Block> EFFECTIVE_ON = Sets.newHashSet(Blocks.CAKE);
 
     public ItemCakeKnife() {
-        super(ToolMaterial.IRON, EFFECTIVE_ON);
+        super(ToolMaterial.IRON, Sets.newHashSet());
     }
 
     @Override
@@ -30,5 +28,11 @@ public class ItemCakeKnife extends ItemCraftTool {
     @Nonnull
     public Multimap<String, AttributeModifier> getAttributeModifiers(@Nonnull EntityEquipmentSlot slot, ItemStack stack) {
         return HashMultimap.create();
+    }
+
+    @Override
+    public float getStrVsBlock(ItemStack stack, IBlockState state) {
+        Material material = state.getMaterial();
+        return material == Material.CAKE ? this.efficiencyOnProperMaterial : super.getStrVsBlock(stack, state);
     }
 }
