08acf78e5c384503e58023c2a16d373715e200ed
GirafiStudios <girafi0@gmail.com>
2017-07-08 00:33:38 +0200

    Moved over to json recipes Recipes now work again, woo!

--- src/main/java/com/girafi/culinarycultivation/init/recipes/Recipes.java
+++ src/main/java/com/girafi/culinarycultivation/init/recipes/Recipes.java
@@ -5,21 +5,18 @@ import com.girafi.culinarycultivation.api.crafting.IWinnowingMachineHandler;
 import com.girafi.culinarycultivation.item.ItemModFishFood.FishType;
 import com.girafi.culinarycultivation.item.ItemModMeatFood.MeatType;
 import com.girafi.culinarycultivation.util.FuelHandler;
-import net.minecraft.block.Block;
+import com.girafi.culinarycultivation.util.RecipeGenerator;
 import net.minecraft.block.BlockDoublePlant;
 import net.minecraft.block.BlockPlanks;
 import net.minecraft.block.BlockTallGrass;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.oredict.OreDictionary;
 
 import javax.annotation.Nonnull;
 
-import static com.girafi.culinarycultivation.init.ModBlocks.*;
 import static com.girafi.culinarycultivation.init.ModItems.*;
 import static com.girafi.culinarycultivation.item.ItemCropProduct.ProductType;
 
@@ -37,36 +34,6 @@ public class Recipes {
     }
 
     private static void addRecipes() {
-        //Machines
-        addShaped(FAN_HOUSING, "PPP", "PRD", "I  ", 'P', "plankWood", 'R', "blockRedstone", 'D', Blocks.DISPENSER, 'I', "ingotIron");
-        addShaped(SEPARATOR, "PHP", "PBF", "  I", 'H', Blocks.HOPPER, 'P', "plankWood", 'B', Items.BOWL, 'F', Blocks.IRON_BARS, 'I', "ingotIron");
-        addShaped(CAULDRON, "   ", "ICI", " I ", 'C', Items.CAULDRON, 'I', "nuggetIron");
-        addShaped(SEED_BAG, " S ", "CXC", " C ", 'C', new ItemStack(Blocks.CARPET, 1, OreDictionary.WILDCARD_VALUE), 'S', "string", 'X', new ItemStack(CROP_SEEDS, 1, OreDictionary.WILDCARD_VALUE));
-
-        //Tools, armor and other stuff
-        addShaped(CANE_KNIFE, " II", " I ", " H ", 'H', TOOL_HANDLE, 'I', "ingotIron");
-        addShaped(FARMER_BOOTS, " B ", "LLL", 'B', Items.LEATHER_BOOTS, 'L', Items.LEATHER);
-        addShaped(FARMER_OVERALLS, "S S", "#L#", 'S', "string", 'L', Items.LEATHER_LEGGINGS, '#', "dyeLightBlue");
-        addShaped(FARMER_SHIRT, "# #", "###", "###", '#', new ItemStack(Blocks.WOOL, 1, 14));
-        addShaped(FARMER_STRAWHAT, "SHS", "W W", 'W', "cropWheat", 'H', Blocks.HAY_BLOCK, 'S', "string");
-        addShaped(HOE_LARGE_DIAMOND, "###", " H ", "H  ", 'H', TOOL_HANDLE, '#', "gemDiamond");
-        addShaped(HOE_LARGE_GOLDEN, "###", " H ", "H  ", 'H', TOOL_HANDLE, '#', "ingotGold");
-        addShaped(HOE_LARGE_IRON, "###", " H ", "H  ", 'H', TOOL_HANDLE, '#', "ingotIron");
-        addShaped(HOE_LARGE_STONE, "###", " H ", "H  ", 'H', TOOL_HANDLE, '#', "cobblestone");
-        addShaped(HOE_LARGE_WOODEN, "###", " H ", "H  ", 'H', TOOL_HANDLE, '#', "plankWood");
-        addShaped(KITCHEN_KNIFE, "  I", " I ", "H  ", 'H', TOOL_HANDLE, 'I', "ingotIron");
-        addShaped(MEAT_CLEAVER, " II", " II", "H  ", 'H', TOOL_HANDLE, 'I', "ingotIron");
-        addShaped(STORAGE_JAR, " S ", "P P", " P ", 'P', "paneGlass", 'S', "slabWood");
-        addShaped(TOOL_HANDLE, "S", "S", 'S', "stickWood");
-
-        //Food
-        addShapeless(new ItemStack(FISH, 1, FishType.FILLET.getMetadata()), new ItemStack(KITCHEN_KNIFE, 1, OreDictionary.WILDCARD_VALUE), "filletFish");
-        addShapeless(new ItemStack(MEAT, 1, MeatType.PATTY.getMetadata()), new ItemStack(MEAT_CLEAVER, 1, OreDictionary.WILDCARD_VALUE), "foodMincedMeat");
-        addShapeless(new ItemStack(MEAT, 3, MeatType.BACON.getMetadata()), new ItemStack(KITCHEN_KNIFE, 1, OreDictionary.WILDCARD_VALUE), new ItemStack(Items.PORKCHOP));
-        addShapeless(new ItemStack(MEAT, 2, MeatType.CHICKEN_NUGGET.getMetadata()), new ItemStack(KITCHEN_KNIFE, 1, OreDictionary.WILDCARD_VALUE), new ItemStack(Items.CHICKEN));
-        addShapeless(new ItemStack(MEAT, 3, MeatType.SQUID_RING.getMetadata()), new ItemStack(KITCHEN_KNIFE, 1, OreDictionary.WILDCARD_VALUE), new ItemStack(MEAT, 1, MeatType.SQUID_MANTLE.getMetadata()));
-        addShapeless(new ItemStack(MEAT, 2, MeatType.SQUID_RING.getMetadata()), new ItemStack(KITCHEN_KNIFE, 1, OreDictionary.WILDCARD_VALUE), new ItemStack(FISH, 1, FishType.SMALL_SQUID.getMetadata()));
-        addShapeless(new ItemStack(CHEESE_SLICE, 7), new ItemStack(KITCHEN_KNIFE, 1, OreDictionary.WILDCARD_VALUE), CHEESE);
         //addShapeless(STORAGE_JAR, 3, StorageJarType.RENNET.getMetaData()), new ItemStack(KITCHEN_KNIFE, 1, OreDictionary.WILDCARD_VALUE), CALF_BELLY, STORAGE_JAR, STORAGE_JAR, STORAGE_JAR, Items.WATER_BUCKET));
 
         //Crop -> Seed recipes
@@ -121,17 +88,11 @@ public class Recipes {
         winnowing.addRecipe(new ItemStack(Items.WHEAT), new ItemStack(Items.WHEAT_SEEDS), 15, new ItemStack(CHAFF_PILE), 90);
     }
 
-    private static void addShaped(Block result, Object... recipe) {
-        addShaped(new ItemStack(result), recipe);
-    }
-
-    private static void addShaped(Item result, Object... recipe) {
-        addShaped(new ItemStack(result), recipe);
-    }
-
     private static void addShaped(@Nonnull ItemStack result, Object... recipe) {
+        RecipeGenerator.createShapedRecipe(result, recipe); //TODO Run only in dev
     }
 
     private static void addShapeless(@Nonnull ItemStack result, Object... recipe) {
+        RecipeGenerator.createShapelessRecipe(result, recipe); //TODO Run only in dev
     }
 }
